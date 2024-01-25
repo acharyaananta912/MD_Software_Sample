5110014eee1ed108af850c54fa7a16a4261b0cbe
Bibek Acharya <acharyaananta912@gmail.com>
2024-01-25 13:13:33 -0600

    commit

---
+++ lab01/test3.c
@@ -0,0 +1,14 @@
+#include<stdlib.h>
+#include<stdio.h>
+
+// Test c program.
+// Insert your name where indicated below.  
+// This code will be compiled and run when you execute the test2.sh script.
+
+int main()
+{
+   printf("Code run by  **insert your name here**\n");
+   printf("Successfully ran C program\n");
+
+   return 0;
+}
