d3bac2d48cbb64fc4bea23e95cf0f32497271af7
Bibek Acharya <acharyaananta912@gmail.com>
2024-01-25 14:24:40 -0600

    Added secant square root method in myfunc.py

---
+++ lab02/myfunc.py
@@ -0,0 +1,24 @@
+def sqrt(x,s=1.0,kmax=10,printshow=False):
+
+    for k in range(kmax):
+        if printshow:
+            print("Before iteration %s, s = %20.15f" % (k,s))
+        s = 0.5*(s + x/s);
+    return s
+
+def sqrt_secant(x, s=1, t=1, kmax=10, printshow=True):
+
+    for k in range(kmax):
+        if printshow:
+            print("Before iteration %s, s=%20.15f" % (k,s))
+        s = (x + s*t)/(s + t)
+    return s
+
+if __name__ == "__main__":
+    
+    print("Using Newton Square Root Method:")
+    s = sqrt(15,4,10,True);
+
+    print("Using Secant Square Root Method:")
+
+    s = sqrt_secant(15,3,4,10,True)
